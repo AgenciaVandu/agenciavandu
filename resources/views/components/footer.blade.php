4c0a7e9f4381bd17606f5cccc7e361fc0e178fb2
Alvar Buenfil <alvarbuenfil@MacBook-Air-de-Alvar.local>
2024-01-31 17:07:24 -0600

    formulario

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,15 +1,18 @@
 <footer class="footer">
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-4 col-md-12 col-sm-12 ">
-                <h6>Nuestra base está en Mérida México, <span>pero nuestros clientes están en todas partes</span>. Utilizamos metodologías que nos permiten trabajar exitosamente a distancia. Somos la agencia de desarrollo diseño y desarrollo web que estabas buscando.</h6>
+            <div class="col-lg-5 col-md-12 col-sm-12 ">
+                <x-form></x-form>
+            </div>
+            <div class="col-lg-4 col-md-12 col-sm-12"></div>
+            <div class="col-lg-3 col-md-12 col-sm-12"></div>
+            <div class="col-12 copy_complet">
+                <h6>Nuestra base está en Mérida México, <span>pero nuestros clientes están en todas partes.</span> Utilizamos metodologías que nos permiten trabajar exitosamente a distancia. Somos la agencia de desarrollo diseño y desarrollo web que estabas buscando.</h6>
                 <div class="copy">
                     <img src="{{asset('img/icono-vandu.svg')}}" class="14" alt="icono agencia vandu">
                     © 2024
                 </div>
             </div>
-            <div class="col-lg-4 col-md-12 col-sm-12"></div>
-            <div class="col-lg-4 col-md-12 col-sm-12"></div>
         </div>
     </div>
 </footer>
---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,43 @@
+<h5 class="pb-3">{{$ftitle ?? '¿Podemos hacer algo por ti?'}}</h5>
+<form action="">
+    <div class="row">
+        <div class="col-6">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Nombre">
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Apellido">
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="form-group mt-2">
+                <input type="tel" class="form-control" placeholder="Teléfono">
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="form-group mt-2">
+                <input type="email" class="form-control" placeholder="Correo electrónico">
+            </div>
+        </div>
+        <div class="col-6">
+            <select class="form-control mt-2" value="Servicio de interés">
+                <option selected>Servicio de interés</option>
+                <option>Web, diseño o rediseño de sitio web</option>
+                <option>Fotografía de interiores o alimentos.</option>
+                <option>Diseño digital (Equipos de diseño on demand)</option>
+                <option>Diseño de impresos o empaque </option>
+            </select>
+        </div>
+        <div class="col-6">
+            <select class="form-control mt-2" value="Servicio de interés">
+                <option selected>Presupuesto</option>
+                <option>> 30,0000 MXN (1.7K USD(</option>
+                <option>$50,000 - $90,000 MXN (2.9K - 5.2K USD)</option>
+                <option>$50,000 - $90,000 MXN (2.9K - 5.2K USD)</option>
+                <option>Diseño de impresos o empaque </option>
+            </select>
+        </div>
+    </div>
+</form>
