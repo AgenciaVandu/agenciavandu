9d9fcd465be1a59dbf6de4e52fef7eec91860827
alvar <alvarbu@gmail.com>
2026-02-19 23:06:19 -0600

    se actualiza el build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss', 'resources/scss/global.scss', 'resources/scss/index.scss', 'resources/scss/servicios.scss', 'resources/scss/contacto.scss', 'resources/scss/branding.scss', 'resources/scss/multimedia.scss', 'resources/scss/construccion.scss',  'resources/js/app.js', 'resources/js/bg.js',  'resources/js/observer.js', 'resources/js/portafolio.js', 'resources/js/diseno.js', 'resources/js/bg.js'],
+            input: ['resources/scss/app.scss', 'resources/scss/global.scss', 'resources/scss/index.scss', 'resources/scss/servicios.scss', 'resources/scss/contacto.scss', 'resources/scss/branding.scss', 'resources/scss/multimedia.scss', 'resources/scss/construccion.scss',  'resources/js/app.js', 'resources/js/bg.js',  'resources/js/observer.js', 'resources/js/portafolio.js', 'resources/js/diseno.js', 'resources/js/bg1.js'],
             refresh: true,
         }),
     ],
