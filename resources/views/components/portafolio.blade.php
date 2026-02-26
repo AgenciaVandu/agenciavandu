f3ddd55fb0ccd502c4da408350ac69ae078f42a7
alvar <alvarbu@gmail.com>
2026-02-26 14:08:10 -0600

    cambios en reproduccion de contenido en servicios web

--- resources/views/components/portafolio.blade.php
+++ resources/views/components/portafolio.blade.php
@@ -1,44 +1,44 @@
 <section class="portafolio">
     <div class="carrusel">
         <div class="item">
-            <video width="620" height="360" autoplay loop muted alt="Portafolio vandu">
+            <video playsinline loop webkit-playsinline preload="metadata" width="620" height="360" autoplay muted alt="Portafolio vandu">
                 <source src="{{asset('/img/portafolio/web/rewear.mp4')}}" type="video/mp4">
             </video>
         </div>
         <div class="item">
-            <video width="620" height="360" autoplay loop muted alt="Portafolio vandu">
+            <video playsinline loop webkit-playsinline preload="metadata" width="620" height="360" autoplay muted alt="Portafolio vandu">
                 <source src="{{asset('/img/portafolio/web/osorio.mp4')}}" type="video/mp4">
             </video>
         </div>
         <div class="item">
             <video width="208" height="260" autoplay loop muted alt="Portafolio vandu">
-                <source src="{{asset('/img/portafolio/web/rewear.mov')}}" autoplay type="video/mp4">
+                <source playsinline loop webkit-playsinline preload="metadata" src="{{asset('/img/portafolio/web/rewear.mov')}}" autoplay type="video/mp4">
             </video>
         </div>
         <div class="item">
-            <video width="620" height="360" autoplay loop muted alt="Portafolio vandu">
+            <video playsinline loop webkit-playsinline preload="metadata" width="620" height="360" autoplay muted alt="Portafolio vandu">
                 <source src="{{asset('/img/portafolio/web/nicxa1.mp4')}}" type="video/mp4">
             </video>
         </div>
     </div>
     <div class="carrusel">
         <div class="item">
-            <video width="620" height="360" autoplay loop muted alt="Portafolio vandu">
+            <video playsinline loop webkit-playsinline preload="metadata" width="620" height="360" autoplay muted alt="Portafolio vandu">
                 <source src="{{asset('/img/portafolio/web/rewear.mp4')}}" type="video/mp4">
             </video>
         </div>
         <div class="item">
-            <video width="620" height="360" autoplay loop muted alt="Portafolio vandu">
+            <video playsinline loop webkit-playsinline preload="metadata" width="620" height="360" autoplay muted alt="Portafolio vandu">
                 <source src="{{asset('/img/portafolio/web/osorio.mp4')}}" type="video/mp4">
             </video>
         </div>
         <div class="item">
-            <video width="208" height="260" autoplay loop muted alt="Portafolio vandu">
+            <video playsinline loop webkit-playsinline preload="metadata" width="208" height="260" autoplay muted alt="Portafolio vandu">
                 <source src="{{asset('/img/portafolio/web/rewear.mov')}}" autoplay type="video/mp4">
             </video>
         </div>
         <div class="item">
-            <video width="620" height="360" autoplay loop muted alt="Portafolio vandu">
+            <video playsinline loop webkit-playsinline preload="metadata" width="620" height="360" autoplay muted alt="Portafolio vandu">
                 <source src="{{asset('/img/portafolio/web/nicxa1.mp4')}}" type="video/mp4">
             </video>
         </div>
